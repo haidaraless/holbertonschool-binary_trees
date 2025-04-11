164f16c69d511bd23e29e14db14f4d6c4b9e5ab4
Haidar Alessa <10682@holbertonstudents.com>
2025-04-11 15:31:56 +0000

    fix 14-binary_tree_balance issue.

--- binary_trees.h
+++ binary_trees.h
@@ -32,7 +32,6 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-
 /* function prototypes */
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
